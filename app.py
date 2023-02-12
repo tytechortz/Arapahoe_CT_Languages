e267294df134b65640caa897d2626f0f53b4d241
James Swank <jamesswank@Jamess-Laptop.local>
2023-02-11 23:11:41 -0700

    adds ct map

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ import geopandas as gpd
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
+bgcolor = "#f3f3f1"  # mapbox light map land color
+
 header = html.Div("Arapahoe County Languages by Census Tract", className="h2 p-2 text-white bg-primary text-center")
+
+template = {"layout": {"paper_bgcolor": bgcolor, "plot_bgcolor": bgcolor}}
 
 df = pd.read_csv('ACSDT5Y2021.C16001-2023-02-11T183920.csv')
 df.drop(df.columns[[1,2]], axis=1, inplace=True)
@@ -20,13 +24,21 @@ gdf = gdf.to_crs("epsg:4326")
 gdf = gdf.set_geometry('geometry')
 
 gdf = gdf.drop(gdf.columns[[1,3,4,5,6,7,8,9,10,11,12,13,14,15]], axis=1)
+
+
+
 # print(gdf['TRACTCE20'])
 tracts = gdf['TRACTCE20'].tolist()
 tracts.insert(0,'Label')
 # print(tracts)
 
 df.columns = tracts
+# df = df.T
 # print(df)
+# tgdf = gdf.merge(df)
+# df.columns = [df.iloc[0]]
+# df = df[2:]
+print(df)
 
 columnDefs = []
 
@@ -35,6 +47,20 @@ def get_ct():
         columnDefs.append({'headerName': x, 'field': x}) 
 
 get_ct()
+
+def blank_fig(height):
+    """
+    Build blank figure with the requested height
+    """
+    return {
+        "data": [],
+        "layout": {
+            "height": height,
+            "template": template,
+            "xaxis": {"visible": False},
+            "yaxis": {"visible": False},
+        },
+    }
 
 # columnDefs = [
 #     {
@@ -76,9 +102,33 @@ app.layout = dbc.Container(
         header,
         # dbc.Row([dbc.Col('ct-grid')]),
         dbc.Row(dbc.Col(table, className="py-4")),
-        dbc.Row(dcc.Graph(id='ct'))
+        dbc.Row(dcc.Graph(id='ct-map', figure=blank_fig(500)))
     ],
 )
+
+@app.callback(
+    Output('ct-map', 'figure'),
+    Input('ct-grid', 'selectionChanged'),
+)
+def update_ct_map(selected_row):
+    if selected_row is None:
+
+        fig = px.choropleth_mapbox(gdf, 
+                                    geojson=gdf.geometry, 
+                                    color="TRACTCE20",                               
+                                    locations=gdf.index, 
+                                    # featureidkey="properties.TRACTCE20",
+                                    opacity=0.5)
+
+    fig.update_layout(mapbox_style="carto-positron", 
+                      mapbox_zoom=10.4,
+                      mapbox_center={"lat": 39.65, "lon": -104.8},
+                      margin={"r":0,"t":0,"l":0,"b":0},
+                      uirevision='constant')
+
+    return fig
+        
+
 
 if __name__ == "__main__":
     app.run_server(debug=True, port=8060)
